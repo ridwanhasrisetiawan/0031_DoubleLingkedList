465d2ca527f09fa5abbd0fb232078fe1dd3e1701
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-15 11:10:28 +0700

    step 9c,9d

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -63,6 +63,12 @@ public:
 
                 newNode->next = current->next;
                 newNode->prev = current;
+
+                if (current->next != NULL)
+                current->next->prev = newNode;
+
+
+                current->next = newNode;
         }
     }
 };
