88053e67305e3210d70d17e20f4526743809fb32
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-05-15 11:05:00 +0700

    commit8

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -48,6 +48,18 @@ public:
 
                 START = newNode;
                 return;
+
+                Node *current = START;
+                while (current->next !=NULL && current->next->noMhs < nim)
+                {
+                    current = current->next;
+                }
+
+                if (current->next !=NULL && nim == current->next->noMhs)
+                {
+                    cout << "\nDuplicate roll number not allowed" << endl;
+                    return;
+                }
         }
     }
 };
